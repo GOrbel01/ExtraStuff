0dc4436a243bf1dc17da80e80db300c93cde6059
George O <georgeojava@gmail.com>
2014-10-14 15:16:02 +0100

    Added papersizes from at uni

---
+++ RecursionPractice/src/PaperSizes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by gorbel01 on 14/10/2014.
+ */
+public class PaperSizes {
+    public static void main(String[] args)
+    {
+        boolean isValid = false;
+        while (isValid)
+        {
+            Scanner keyboard = new Scanner(System.in);
+            String paperSize = keyboard.nextLine();
+            if (paperSize.charAt(0) == 'A')
+            {
+
+            }
+        }
+
+    }
+}
